0b23efc069a6d6aefe9db96283e4e041b9410fc8
Vedant Kadam <vedant.kadam@neosofttech.com>
2026-08-31 14:36:30 +0530

    Using DefaultCredentialsProvider

--- src/main/java/com/neosoft/order_service/config/AwsConfig.java
+++ src/main/java/com/neosoft/order_service/config/AwsConfig.java
@@ -3,7 +3,7 @@ package com.neosoft.order_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sns.SnsClient;
@@ -18,7 +18,7 @@ public class AwsConfig {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -26,7 +26,7 @@ public class AwsConfig {
     public SnsClient snsClient() {
         return SnsClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
